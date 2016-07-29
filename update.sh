c974cc87fece3f31ec13c021902f91984cd14bef
Rangi <rangi@Rangi-iMac.local>
2016-07-29 13:56:27 +0900

    updater script working!

--- update.sh
+++ update.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+
+source ~/.keychain/$COMPUTER_NAME.local-sh
+
+pushd $HOME/repos/osx > /dev/null
+
 git pull
 brew update && brew upgrade
 cp -r "dotfiles/". "$HOME/"
+
+popd > /dev/null
